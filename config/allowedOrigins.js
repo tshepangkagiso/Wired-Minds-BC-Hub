6ac8c0562c09682af1a17b68576d2c8bfcb14bf2
tshepangkagiso <tshepangkagisomashigo8@gmailcom>
2023-11-30 13:52:02 +0200

    version 1.1.2

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins=[
     'http://localhost:3500',
-    'https://www.wiredmindsbc.com',
-    'https://wiredmindsbc.com'
+    'https://wired-bc.onrender.com',
+    'https://www.wired-bc.onrender.com'
 
 ]// this are the only domains allowed to access this api
 
