1b33f2a8a4d924b9064e60c8537c690201a5d665
tshepangkagiso <tshepangkagisomashigo8@gmailcom>
2023-12-03 10:28:07 +0200

    version 1.1.6.2

--- middleware/backgroundjobs/cronIndex.js
+++ middleware/backgroundjobs/cronIndex.js
@@ -8,8 +8,8 @@ const apiIndex = 'https://wired-bc.onrender.com';
 
 // Function to create a cron job for the first API endpoint
 const CronIndex1 = (apiIndex) => {
-    // Schedule the task to call the API endpoint every 15 minutes
-    cron.schedule('*/15 * * * *', () => {
+    // Schedule the task to call the API endpoint every 12 minutes
+    cron.schedule('*/12 * * * *', () => {
         // Make a request to the API
         fetch(apiIndex)
             .then(response => {
@@ -26,6 +26,11 @@ const CronIndex1 = (apiIndex) => {
             });
     });
 }
+
+/*
+Render spins down a Free web service that goes 15 minutes without receiving inbound traffic. Render spins the service back up whenever it next receives a request to process.
+Spinning up a service takes a few seconds, which causes a noticeable delay for incoming requests until the service is back up and running. For example, a browser page load will hang momentarily.
+ */
 
 
 // Export the cron job functions
